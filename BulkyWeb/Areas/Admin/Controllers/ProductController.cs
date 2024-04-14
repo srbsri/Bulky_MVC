f4192beb6b9089392f1918b56476de07858cf617
Saurabh Srivastava <srb.sri25@gmail.com>
2024-04-14 17:12:57 +0530

    Lec94 (Add Upsert)

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -29,41 +29,36 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
 
             return View(objProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-            //   .GetAll().Select(u => new SelectListItem
-            //   {
-            //       Text = u.Name,
-            //       Value = u.Id.ToString()
-            //   });
-            //ViewBag.CategoryList = CategoryList;
-            //ViewData["CategoryList"] = CategoryList;
             ProductVM productVM = new()
             {
                 CategoryList = _unitOfWork.Category
-                .GetAll().Select(u => new SelectListItem
-                {
-                    Text = u.Name,
-                    Value = u.Id.ToString()
-                }),
-            Product = new Product()
+                   .GetAll().Select(u => new SelectListItem
+                   {
+                       Text = u.Name,
+                       Value = u.Id.ToString()
+                   }),
+                Product = new Product()
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                // create
+                return View(productVM);
+            }
+            else
+            {
+                //for update
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+
         }
         [HttpPost]
-        //public IActionResult Create(Product obj)
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
-            
             if (ModelState.IsValid)
             {
-                //_db.Categories.Add(obj); //what it has to add
-                //_db.SaveChanges();  //go to the database and create the product
-                //_productRepo.Add(obj); 
-                //_productRepo.Save();
-
-                //_unitOfWork.Product.Add(obj);
                 _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product Created Successfully";
@@ -79,47 +74,9 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 });
                 TempData["error"] = "There is some Error";
                 return View(productVM);
+
             }
         }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //Product? productFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
-            //Product? productFromDb1 = _db.Categories.Find(id);
-            //Product? productFromDb2 = _db.Categories.Where(u => u.Id == id).FirstOrDefault;
-
-            //Product? productFromDb = _productRepo.Get(u=> u.Id==id);
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-            if (ModelState.IsValid)
-            {
-                //_db.Categories.Update(obj); //what it has to add
-                //_db.SaveChanges();       //go to the database and create the product
-                //_productRepo.Update(obj);
-                //_productRepo.Save();
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
-
-
 
         public IActionResult Delete(int? id)
         {
